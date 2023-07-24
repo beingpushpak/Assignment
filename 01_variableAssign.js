517d5d66a7dcdb160806b55edcf3d2698e059d91
beingpushpak <beingpushpak@gmail.com>
2023-07-24 13:33:47 +0530

    Create 01_variableAssign.js

---
+++ 01_variableAssign.js
@@ -0,0 +1,28 @@
+//part 1
+//declairation
+var myFullName;
+console.log("type of variable myFullName is "+ myFullName);
+//initialization
+myFullName = "Pushpak Metkari";
+console.log("first and last name is = "+ myFullName);
+//updating the value of myFullName 
+myFullName = "Pushpak Narayan Metkari";
+console.log("my fullname is = "+ myFullName);
+//part 2
+var myLove = "i love JS";
+console.log(myLove);
+//part 3
+var firstName ="Pushpak";
+console.log("The first name is = "+ firstName);
+var lastName = "Metkari";
+console.log("last name is = "+ lastName);
+var age = 77;
+console.log("the age is = "+age);
+var address = "INDIA";
+console.log("my address is = "+address);
+var pinCode = 112233;
+console.log("the pin code is = "+pinCode);
+var gender = "Male";
+console.log("Gender is = "+gender);
+var isMarried = false;
+console.log("are you married? = "+ isMarried);
