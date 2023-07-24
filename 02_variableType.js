90c5dac48e83fd35ca2a1bb6cd7275433d5ed32d
beingpushpak <beingpushpak@gmail.com>
2023-07-24 13:33:54 +0530

    Create 02_variableType.js

---
+++ 02_variableType.js
@@ -0,0 +1,28 @@
+//part 1
+//variable declaration
+var bankName;
+console.log("Type of variable bankname is = "+ bankName);
+//initiolization
+bankName="SBI BANK";
+console.log("Bank name is = "+ bankName);
+console.log("Type of variable bankname is = "+ typeof bankName );
+//part 2
+//declaration and initilization on same line
+var marks = '90%';
+console.log("mark is = "+ marks);
+console.log("type of variable mark is ="+ typeof marks);
+//part 3
+var isWorking;
+console.log("Type of variable isWorking is = "+ typeof isWorking);
+isWorking = false;
+console.log("are you working? "+ isWorking);
+console.log("Type of variable isWorking is = "+ typeof isWorking);
+//part 4
+var totalCount = "one hundred and seven";
+console.log("the total count is "+ totalCount);
+console.log("Type of variable totalCount is = "+ typeof totalCount);
+totalCount=107;
+console.log("the total count is "+ totalCount);
+console.log("Type of variable totalCount is = "+ typeof totalCount);
+
+
