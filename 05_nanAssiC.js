9c739d5b74652090a698db84e9ef706ad17d6f07
beingpushpak <beingpushpak@gmail.com>
2023-07-27 13:50:19 +0530

    Create 05_nanAssiC.js

---
+++ 05_nanAssiC.js
@@ -0,0 +1,43 @@
+console.log(`------------------Assingnment---------------------`);
+console.log(`------------------part-1---------------------`);
+
+var result = 0=='';
+console.log(`the result of 0 =='' is ${result}`);
+console.log(`it is true because of internally '' is convert in '0' and which is '0' == 0 is true`);
+
+console.log(`------------------part-2---------------------`);
+
+var result = 0=='0';
+console.log(`the result of 0 =='0' is ${result}`);
+console.log(`it is true because of internally '0' is convert in 0 and which is 0 == 0 is true`);
+
+console.log(`------------------part-3---------------------`);
+var result = 0==false;
+console.log(`the result of 0 == false is ${result}`);
+console.log(`it is true because of in internally false convert into numbrical value which is 0 therefore 0 == 0 which is true`);
+
+console.log(`------------------part-4---------------------`);
+
+var result = null==undefined;
+console.log(`the result of null == undefined is ${result}`);
+console.log(`it is true because of null is empty which does not hold any value also undefine does not hold any value thats why it is true`);
+
+console.log(`------------------part-5---------------------`);
+
+var result = 1==[1];
+console.log(`the result of 1==[1] is ${result}`);
+console.log(`it is true because of [1] is a arrey and hold a single value which is 1 and in == oparator 1 == 1 is true`);
+
+console.log(`------------------part-6---------------------`);
+
+var result = 1==true;
+console.log(`the result of 1 == true is ${result}`);
+console.log(`it is true because of in internally true convert into numbrical value which is 1 therefore 1 == 1 which is true`);
+
+console.log(`------------------part-7---------------------`);
+
+var result = 1=='1';
+console.log(`the result of 1 == '1' is ${result}`);
+console.log(`it is true because of in internally '1' convert into numbrical value which is 1 therefore 1 == 1 which is true`);
+
+
