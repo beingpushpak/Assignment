b28decd8eb5deddb5cf75a5dfb8b1839d97bd43c
beingpushpak <beingpushpak@gmail.com>
2023-07-31 09:24:57 +0530

    Assignment for for and while loop

---
+++ 06_forLoopAssignment.js
@@ -0,0 +1,43 @@
+console.log(`------------------------Assignment-1------------------------------`);
+console.log(`------------------------part-1-------------------------`);
+console.log(`number from 5 to 15 is`);
+for (let index = 5; index <=15; index++) {
+    console.log(index);
+    
+}
+console.log(`------------------------part-2-------------------------`);
+console.log(`the number from 50 to 40 is`);
+for (let index = 50; index >=40; index--) {
+    console.log(index);
+    
+}
+console.log(`------------------------part-3-------------------------`);
+console.log(`First 15 odd number is`);
+for (let index = 1; index < 30; index=index+2) {
+    console.log(index);
+    
+}
+console.log(`------------------------part-4-------------------------`);
+console.log(`First 10 even number is`);
+for (let index = 2; index <= 20; index=index+2) {
+    console.log(index);
+    
+}
+console.log(`------------------------part-5-------------------------`);
+console.log(`Table of 5 is`);
+for (let index = 5; index <= 50; index=index+5) {
+    console.log(index);
+    
+}
+console.log(`------------------------part-6-------------------------`);
+console.log(`Table of 10 is`);
+for (let index = 10; index <=100; index=index+10) {
+    console.log(index);
+    
+}
+console.log(`------------------------part-7-------------------------`);
+console.log(`Table of 10 in reverse manner`);
+for (let index = 100; index >=10; index=index-10) {
+    console.log(index);
+    
+}
---
+++ 06_whileLoop.js
@@ -0,0 +1,50 @@
+console.log(`----------------------Assignment-1-----------------------------`);
+console.log(`----------------------part-1-----------------------------`);
+console.log(`Number start from 5 to 15 is`);
+var i=5;
+while (i<=15) {
+    console.log(i);
+    i++;
+}
+console.log(`----------------------part-2-----------------------------`);
+console.log(`Number start from 50 to 40 is`);
+var i=50;
+while (i>=40) {
+    console.log(i);
+    i--;
+}
+console.log(`----------------------part-3-----------------------------`);
+console.log(`First  15 odd number is`);
+var i=1;
+while (i<=29) {
+    console.log(i);
+    i=i+2;
+}
+console.log(`----------------------part-4-----------------------------`);
+console.log(`First 10 Eeven number is`);
+var i=2;
+while (i<=20) {
+    console.log(i);
+    i=i+2;
+}
+console.log(`----------------------part-5-----------------------------`);
+console.log(`Table of 5 is`);
+var i=5;
+while (i<=50) {
+    console.log(i);
+    i=i+5;
+}
+console.log(`----------------------part-6-----------------------------`);
+console.log(`Table of 10 is`);
+var i=10;
+while (i<=100) {
+    console.log(i);
+    i=i+10;
+}
+console.log(`----------------------part-7-----------------------------`);
+console.log(`Table of 10 in reverse manner is`);
+var i=100;
+while (i>=10) {
+    console.log(i);
+    i=i-10;
+}
